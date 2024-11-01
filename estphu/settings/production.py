198b9e947adb424d33cf847db03053c73fb65e79
Estphu <jaydee20001217@gmail.com>
2024-11-02 02:10:22 +0500

    bug(git-log): test

---
+++ estphu/settings/production.py
@@ -0,0 +1,19 @@
+from .base import *
+
+DEBUG = False
+ALLOWED_HOSTS = ["domain.com"]
+
+
+# Database
+# https://docs.djangoproject.com/en/5.1/ref/settings/#databases
+
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": env("DB_NAME"),
+        "USER": env("DB_USER"),
+        "PASSWORD": env("DB_PASSWORD"),
+        "HOST": env("DB_HOST", default="localhost"),
+        "PORT": env("DB_PORT", default="5432"),
+    }
+}
