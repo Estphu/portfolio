5769982dc1667b7b4aef88044bb2bc62321b69a4
Estphu <jaydee20001217@gmail.com>
2024-11-08 19:55:20 +0500

    chore: HTMX and Alpine.js Initialized

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns = [path("", views.IndexView, name="index")]
+urlpatterns = [
+    path("", views.IndexView, name="index"),
+    path("load-more/", views.load_more_content, name="load_more_content"),
+]
--- core/views.py
+++ core/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
 # Create your views here.
 def IndexView(request):
-    return render(request, "core/base.html")
+    return render(request, "core/home.html")
+
+
+def load_more_content(request):
+    # Content returned by HTMX
+    return HttpResponse(
+        "<p class='p-4 bg-green-100 rounded'>Here's some more content loaded dynamically with HTMX!</p>"
+    )
